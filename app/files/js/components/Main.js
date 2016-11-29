a342a6dbc55e52c55a8d4e69b4b2ad6c7c10b74c
Edgar Pino <edgarbeltran@me.com>
2016-11-29 00:14:45 -0600

    created header

--- app/files/js/components/Main.js
+++ app/files/js/components/Main.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {deepOrange500} from 'material-ui/styles/colors';
+import {deepOrange500,blueGrey200,brown100} from 'material-ui/styles/colors';
 
 import HeaderNav from './parts/HeaderNav';
 
 const muiTheme = getMuiTheme({
     palette: {
         accent1Color: deepOrange500,
+    },
+    appBar: {
+        color: blueGrey200,
     },
 });
 
--- app/files/js/components/parts/HeaderNav.js
+++ app/files/js/components/parts/HeaderNav.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
-
 const HeaderNav = () => (
-    <AppBar
-        title="EP"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-    />
+    <header className="main--header full-height">
+        <AppBar
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <figure>
+            <figcaption className="header--wrapper">
+                <div className="header--fill">
+                    <iframe width="100%" height="300px" allowfullscreen="" frameborder="0" src="//storage.googleapis.com/vrview/2.0/index.html?image=//storage.googleapis.com/vrview/examples/coral.jpg&is_stereo=true&is_vr_off"></iframe>
+                    <h1 className="header--title">&lt; Hi, my name Edgar Pino /&gt;</h1>
+                </div>
+            </figcaption>
+        </figure>
+    </header>
+
+
 );
 
 export default HeaderNav;
