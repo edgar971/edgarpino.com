df2a96ca54726af80380bd790f8f7bd23a9e0d7f
Edgar Pino <edgarbeltran@me.com>
2016-12-01 22:41:53 -0600

    Worked on header and fixed css issues

--- app/files/js/components/parts/HeaderNav.js
+++ app/files/js/components/parts/HeaderNav.js
@@ -9,6 +9,7 @@ const HeaderNav = () => (
         <figure>
             <figcaption className="header--wrapper">
                 <div className="header--fill">
+                    <img  className="round-img profile--image padding-all-1x" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAc5AAAAJGVmOWQwOTBmLTU3MDQtNGVjOS04YTdhLWViNDIyZjNhMjY5MQ.jpg" alt="Edgar Pino"/>
                     <h1 className="header--title">&lt; Hi, my name Edgar Pino /&gt;</h1>
                 </div>
             </figcaption>
--- app/files/js/components/parts/MainSection.js
+++ app/files/js/components/parts/MainSection.js
@@ -4,10 +4,15 @@ const MainSection = () => (
 
     <main className="main--body container">
         <section className="section padding">
-            <h1>Welcome</h1>
+            <figure>
+                <figcaption className="font-center padding-all-3x">
+                    <h4>
+                        I'm a software engineer from Green Bay, Wisconsin.
+                    </h4>
+                </figcaption>
+            </figure>
         </section>
     </main>
-
 );
 
 export default MainSection;
