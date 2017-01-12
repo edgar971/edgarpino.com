0272a6a7a7dee01870a671373faeda1b61c44bb2
Edgar Pino <me@edgarpino.com>
2017-01-11 23:13:58 -0600

    Removed webpack and react code

--- app/files/js/App.js
+++
@@ -1,7 +0,0 @@
-import MainApp from './components/Main';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-{/*ReactDOM.render(<MainApp />, document.getElementById('app-render'));*/}
-
