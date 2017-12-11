08969db796e8f944051f38a3a821b8433f10fa39
Edgar Pino <edgarbeltran@me.com>
2017-12-11 13:19:55 -0700

    content updates

--- files/js/main.js
+++ files/js/main.js
@@ -18,26 +18,18 @@ skel.breakpoints({
     xsmall: '(max-width: 480px)'
 });
 
-// Disable animations/transitions until the page has loaded.
 $html.addClass('is-loading');
 
 $window.on('load', function() {
-
     $html.removeClass('is-loading');
-
 });
 
 ReactDOM.render(<Form />, document.getElementById('contact-form'));
 
 $(function() {
-
-
-    // Touch mode.
     if (skel.vars.mobile) {
-
         let $wrapper;
 
-        // Create wrapper.
         $body.wrapInner('<div id="wrapper" />');
         $wrapper = $('#wrapper');
 
@@ -52,36 +44,27 @@ $(function() {
             $window.trigger('scroll');
         });
 
-        // Scrolly.
         $window.on('load.hl_scrolly', function() {
-
             $('.scrolly').scrolly({
                 speed: 1000,
                 parent: $wrapper,
                 pollOnce: true
             });
-
             $window.off('load.hl_scrolly');
-
         });
 
         // Enable touch mode.
         $html.addClass('is-touch');
-
     }
     else {
-
         // Scrolly.
         $('.scrolly').scrolly({
             speed: 1000
         });
-
     }
 
-    // Fix: Placeholder polyfill.
     $('form').placeholder();
 
-    // Prioritize "important" elements on medium.
     skel.on('+medium -medium', function() {
         $.prioritize(
             '.important\\28 medium\\29',
@@ -89,18 +72,13 @@ $(function() {
         );
     });
 
-    // Header.
     var $header = $('#header'),
         $headerTitle = $header.find('header'),
         $headerContainer = $header.find('.container');
 
-    // Make title fixed.
     if (!skel.vars.mobile) {
-
         $window.on('load.hl_headerTitle', function() {
-
             skel.on('-medium !medium', function() {
-
                 $headerTitle
                     .css('position', 'fixed')
                     .css('height', 'auto')
@@ -108,11 +86,9 @@ $(function() {
                     .css('left', '0')
                     .css('width', '100%')
                     .css('margin-top', ($headerTitle.outerHeight() / -2));
-
             });
 
             skel.on('+medium', function() {
-
                 $headerTitle
                     .css('position', '')
                     .css('height', '')
@@ -120,25 +96,18 @@ $(function() {
                     .css('left', '')
                     .css('width', '')
                     .css('margin-top', '');
-
             });
-
             $window.off('load.hl_headerTitle');
-
         });
-
     }
 
     // Scrollex.
     skel.on('-small !small', function() {
         $header.scrollex({
             terminate: function() {
-
                 $headerTitle.css('opacity', '');
-
             },
             scroll: function(progress) {
-
                 let x;
                 // Fade out title as user scrolls down.
                 if (progress > 0.5) {
@@ -146,41 +115,30 @@ $(function() {
                 } else {
                     x = progress;
                 }
-
-
                 $headerTitle.css('opacity', Math.max(0, Math.min(1, x * 2)));
-
             }
         });
     });
 
     skel.on('+small', function() {
-
         $header.unscrollex();
-
     });
 
     // Main sections.
     $('.main').each(function() {
-
         var $this = $(this),
             $primaryImg = $this.find('.image.primary > img'),
             $bg,
             options;
-
         // No primary image? Bail.
         if ($primaryImg.length == 0)
             return;
-
         // Hack: IE8 fallback.
         if (skel.vars.IEVersion < 9) {
-
             $this
                 .css('background-image', 'url("' + $primaryImg.attr('src') + '")')
                 .css('-ms-behavior', 'url("css/ie/backgroundsize.min.htc")');
-
             return;
-
         }
 
         // Create bg and append it to body.
@@ -199,27 +157,20 @@ $(function() {
         };
 
         if (skel.canUse('transition')) {
-
             options.init = function() { $bg.removeClass('active'); };
             options.enter = function() { $bg.addClass('active'); };
             options.leave = function() { $bg.removeClass('active'); };
-
         }
         else {
-
             $bg
                 .css('opacity', 1)
                 .hide();
-
             options.init = function() { $bg.fadeOut(0); };
             options.enter = function() { $bg.fadeIn(400); };
             options.leave = function() { $bg.fadeOut(400); };
-
         }
 
         $this.scrollex(options);
-
     });
-
 });
 
